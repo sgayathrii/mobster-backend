8ed30f7541c77011ff3ba725eb65238afcc072df
Johan Abde <abdejohan@gmail.com>
2023-11-02 15:50:15 +0100

    Fix minor bug

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const mobsRouter = require('./routes/mobs');
 const membersRouter = require('./routes/members');
 var corsOptions = {
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/mobs', mobsRouter);
 app.use('/members', membersRouter);
 
