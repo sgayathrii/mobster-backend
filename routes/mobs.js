04f75c919216d3a9e249791491ed24b64a3ff555
Johan Abde <abdejohan@gmail.com>
2023-11-02 14:54:26 +0100

    Setup CRUD operations for /mobs

--- routes/mobs.js
+++ routes/mobs.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+router.post('/', function (req, res, next) {
+  const { name } = req.body;
+  const id = uuidv4();
+  res.json({ id, name });
+});
+router.put('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+router.delete('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
